3606834c76723e358799e77e806d8eda4deb152e
Nicholas Shephard <shephardn@gmail.com>
2021-12-01 14:31:42 -0600

    commit tree 8067158c2b4610eb4285ecf98a0a7ecaea94c560

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,33 @@
 // create your App component here
+import React, { useEffect, useState } from "react"
+
+function App () {
+
+    const [isRendered, setRendered] = useState(false);
+    const [dogData, setDogData] = useState("");
+
+
+    const API_URL = "https://dog.ceo/api/breeds/image/random"
+  
+
+    useEffect(() => {
+            fetch (API_URL)
+            .then(resp => resp.json())
+            .then(data => {
+                setRendered(true);
+                setDogData(data);
+                
+            })
+    }, []);
+
+    
+
+    return (
+        <div className="App">
+            <p>some text</p>
+            {isRendered ? <img src={dogData.message} alt="A Random Dog" /> : <p>Loading...</p> }
+        </div>
+    )
+
+}
+export default App;
